afef9b8fd18ccf5188c6444fe13c9add716d32c0
AmitaiYacobi <amitaiyacobi@gmail.com>
2022-06-21 17:28:08 +0300

    added some changes

--- merkle.py
+++ merkle.py
@@ -2,8 +2,9 @@ import base64
 import hashlib
 
 from cryptography.exceptions import InvalidSignature
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+
 
 class MerkleTree:
     def __init__(self, hash_function):
@@ -21,7 +22,7 @@ class MerkleTree:
     def update_root(self, level):
         if len(level) == 1:
             self.root = level[0]
-            return 
+            return
         else:
             higher_level = []
             for i in range(0, len(level), 2):
@@ -29,29 +30,28 @@ class MerkleTree:
                     higher_level.append(level[i])
                     break
                 else:
-                    concatenated = "".join([level[i], level[i+1]])
+                    concatenated = "".join([level[i], level[i + 1]])
                     higher_level.append(self.hash(concatenated.encode('utf-8')).hexdigest())
-            
+
             self.update_root(higher_level)
 
     def get_root(self):
         if self.root is None:
-            print("\n")
+            print("")
         else:
             print(self.root)
 
     def get_proof_of_inclusion(self, position):
         position = int(position)
         if position < 0 or position > len(self.leafs) - 1:
-            print("\n")
-            return 
+            print("")
+            return
 
         hash_leafs = [leaf[1] for leaf in self.leafs]
-        proof = []
-        proof.append(self.root)
+        proof = [self.root]
         proof = self.calculate_hashes(hash_leafs, position, proof)
         print(" ".join(proof))
-    
+
     def calculate_hashes(self, level, position, proof):
         if len(level) == 1:
             return proof
@@ -62,17 +62,17 @@ class MerkleTree:
                     higher_level.append(level[i])
                     break
                 else:
-                    concatenated = "".join([level[i], level[i+1]])
+                    concatenated = "".join([level[i], level[i + 1]])
                     higher_level.append(self.hash(concatenated.encode('utf-8')).hexdigest())
             if position % 2 == 0:
                 if position == len(level) - 1:
-                    new_position = len(higher_level) - 1 
+                    new_position = len(higher_level) - 1
                 else:
                     proof.append('1' + level[position + 1])
                     concatenated = "".join([level[position], level[position + 1]])
                     hash_concatenated = self.hash(concatenated.encode('utf-8')).hexdigest()
                     new_position = higher_level.index(hash_concatenated)
-                return self.calculate_hashes(higher_level, new_position, proof)                    
+                return self.calculate_hashes(higher_level, new_position, proof)
             else:
                 proof.append('0' + level[position - 1])
                 concatenated = "".join([level[position - 1], level[position]])
@@ -90,7 +90,7 @@ class MerkleTree:
             print(True)
         else:
             print(False)
-    
+
     def create_keys(self):
         private_key = rsa.generate_private_key(
             public_exponent=65537,
@@ -117,12 +117,12 @@ class MerkleTree:
         line = input()
         private_key = inpt[2:]
         while line != "-----END RSA PRIVATE KEY-----":
-            private_key += '\n'            
+            private_key += '\n'
             private_key += line
             line = input()
-        private_key +='\n'
+        private_key += '\n'
         private_key += line
-        
+
         sign_key = serialization.load_pem_private_key(
             private_key.encode(),
             password=None,
@@ -144,16 +144,23 @@ class MerkleTree:
         line = input()
         public_key = inpt[2:]
         while line != "-----END PUBLIC KEY-----":
-            public_key += '\n'            
+            public_key += '\n'
             public_key += line
             line = input()
-        public_key +='\n'
+        public_key += '\n'
         public_key += line
         line = input()
         while line == '':
             line = input()
-        sign = line 
-        text = input()
+        sign_text = line.split(" ")
+        if len(sign_text) == 2 and sign_text[1] != '':
+            sign = sign_text[0]
+            text = sign_text[1]
+        else:
+            sign = line
+            text = input()
+            while text == '':
+                text = input()
 
         verify_key = serialization.load_pem_public_key(
             public_key.encode(),
@@ -168,17 +175,18 @@ class MerkleTree:
                     mgf=padding.MGF1(hashes.SHA256()),
                     salt_length=padding.PSS.MAX_LENGTH
                 ),
-                 hashes.SHA256()
+                hashes.SHA256()
             )
             print(True)
 
         except InvalidSignature:
             print(False)
 
+
 def do_action(merkle_tree, option, first_line, inpt):
     if option == '1' and len(first_line) > 1:
         merkle_tree.add_leaf(first_line[1])
-    
+
     elif option == '2' and len(first_line) == 1:
         merkle_tree.get_root()
 
@@ -195,25 +203,24 @@ def do_action(merkle_tree, option, first_line, inpt):
         merkle_tree.create_sign_on_root(inpt)
 
     elif option == '7' and len(first_line) > 1:
-        merkle_tree.verify_sign(inpt) 
-    
+        merkle_tree.verify_sign(inpt)
+
     else:
         print('')
 
+
 def main():
     hash_function = hashlib.sha256
-    mekle_tree = MerkleTree(hash_function)
+    merkle_tree = MerkleTree(hash_function)
     while True:
         inpt = input()
         first_line = inpt.split(' ')
-        if (first_line[0] == ''):
+        if first_line[0] == '':
             print('')
         else:
             option = inpt[0]
-            do_action(mekle_tree, option, first_line, inpt)
+            do_action(merkle_tree, option, first_line, inpt)
 
 
 if __name__ == "__main__":
-    # file = open('e.txt', 'r')
-    # print(file.read())
     main()
